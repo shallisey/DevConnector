151bb3dde287d55815bfcfafe661bcee7a054d90
Sean Hallisey <halliseyseanshsu@gmail.com>
2019-08-23 08:56:43 -0500

    Add Get posts, Get post by ID, and Delete post by ID routes

--- routes/api/post.js
+++ routes/api/post.js
@@ -7,7 +7,7 @@ const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
-// @route     POST /api/posts
+// @route     POST /api/post
 // @desc      Create a post
 // @access    private
 router.post(
@@ -45,5 +45,66 @@ router.post(
     }
   }
 );
+
+// @route     GET /api/post
+// @desc      Get all posts
+// @access    private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route     GET /api/post/:id
+// @desc      Get post by ID
+// @access    private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route     DELETE /api/post/:id
+// @desc      Delete a post by ID
+// @access    private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    //check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
